413ff4e0809beba6aab33d2748734eb57c918706
Shawn Feng <42893396+Shawnfeng92@users.noreply.github.com>
2019-08-23 14:46:37 -0500

    Update lpsolve.R

--- lpsolve.R
+++ lpsolve.R
@@ -4,6 +4,7 @@ puzzle <- read.csv(file = "Puzzle.csv", header = FALSE)
 
 # create a vector of length of 729
 objective.in <- rep(1, 729)
+direction <- "min"
 
 # create a rhs vector
 const.rhs <- rep(1, 729)
