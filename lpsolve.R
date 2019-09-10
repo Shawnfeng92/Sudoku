59a70587516702f2789f7f45770716d4c19855bc
Shawn Feng <42893396+Shawnfeng92@users.noreply.github.com>
2019-09-10 15:24:41 -0500

    Update lpsolve.R

--- lpsolve.R
+++ lpsolve.R
@@ -12,26 +12,26 @@ for (i in 0:80) {
   temp <- c(rep(0,i*9), rep(1,9),rep(0,729-(i+1)*9))
   const.mat <- cbind(const.mat, temp)
 }
-# 
-# # creat every line and column constraints
-# for (i in 1:9) {
-#   for (j in 1:9) {
-#     temp <- 2:730
-#     temp[which((temp<=(i*81))&(temp>=(i-1)*81)&((temp-1)%%9==j))] <- 1
-#     temp[which(temp > 1)] <- 0
-#     const.mat  <- cbind(const.mat, temp)
-#     temp <- 2:730
-#     temp[which((temp<=(j*81))&(temp>=(j-1)*81)&((temp-1)%%9==i))] <- 1
-#     temp[which(temp > 1)] <- 0
-#     const.mat  <- cbind(const.mat, temp)
-#   }
-# }
-# 
-# # create every square constraint
-# for (i in 0:8) {
-#   temp <- c(rep(0,i*81), as.vector(diag(1, 9, 9)),rep(0,729-(i+1)*81))
-#   const.mat <- cbind(const.mat, temp)
-# }
+
+# creat every line and column constraints
+for (i in 1:9) {
+  for (j in 1:9) {
+    temp <- 2:730
+    temp[which((temp<=(i*81))&(temp>=(i-1)*81)&((temp-1)%%9==j))] <- 1
+    temp[which(temp > 1)] <- 0
+    const.mat  <- cbind(const.mat, temp)
+    temp <- 2:730
+    temp[which((temp<=(j*81))&(temp>=(j-1)*81)&((temp-1)%%9==i))] <- 1
+    temp[which(temp > 1)] <- 0
+    const.mat  <- cbind(const.mat, temp)
+  }
+}
+
+# create every square constraint
+for (i in 0:8) {
+  temp <- c(rep(0,i*81), as.vector(diag(1, 9, 9)),rep(0,729-(i+1)*81))
+  const.mat <- cbind(const.mat, temp)
+}
 
 rm(i, j, temp)
 
