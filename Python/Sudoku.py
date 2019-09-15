4aa255e9c9a04a6480b5364b9ad6b5d87ec6a90c
Shawn Feng <42893396+Shawnfeng92@users.noreply.github.com>
2019-09-15 09:17:45 -0500

    Update Sudoku.py

--- Python/Sudoku.py
+++ Python/Sudoku.py
@@ -1,2 +1,11 @@
 import pulp
-pulp.pulpTestAll()
+import numpy
+import csv
+
+puzzleFile = "C://Users//Shawn//Documents//GitHub//Sudoku//puzzle.csv"
+with open(puzzleFile, 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+    # get all the rows as a list
+    data = list(reader)
+    # transform data into numpy array
+    data = numpy.array(data).astype(float)
