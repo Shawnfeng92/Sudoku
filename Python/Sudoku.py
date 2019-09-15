9d26642d60e06d4e64f031f6442cc84f8ae78a75
Shawn Feng <42893396+Shawnfeng92@users.noreply.github.com>
2019-09-15 10:10:08 -0500

    Update Sudoku.py

--- Python/Sudoku.py
+++ Python/Sudoku.py
@@ -1,8 +1,13 @@
 import pulp
-import numpy
 import pandas
-
-puzzle = pandas.read_csv("puzzle.csv", header = None)
 
 constraint = pandas.read_csv("constraints.csv", header = "infer")
 
+puzzle = pandas.read_csv("puzzle.csv", header = None)
+
+sudoku = pulp.LpProblem("the sudoku problem", pulp.LpMinimize)
+choices = pulp.LpVariable.dicts("Choice", (729,1,1), 0, 1, pulp.LpInteger)
+sudoku += 0, "Arbitrary Objective Function"
+
+for i in range(0,81*4):
+    sudoku += sum(choices * constraint[[i]]) == 1, ""
