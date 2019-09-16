c834f178d20a1e18946c4508023edd4d3cd176e1
Shawn Feng <42893396+Shawnfeng92@users.noreply.github.com>
2019-09-16 13:18:12 -0500

    Update Sudoku.py

--- Python/Sudoku.py
+++ Python/Sudoku.py
@@ -10,4 +10,12 @@ choices = LpVariable.dicts("choices", list(range(729)), 0, 1, LpInteger)
 sudoku += 0, "Arbitrary Objective Function"
 
 for i in range(0,81*4):
-    sudoku += sum(choices[0] * ([1]*729)) == 1, ""
+    sudoku += sum(choices[j] * constraint.iloc[j,i] for j in range(729)) == 1, ""
+    
+# The problem is solved using PuLP's choice of Solver
+sudoku.solve()
+
+# The status of the solution is printed to the screen
+print("Status:", LpStatus[sudoku.status])
+
+
